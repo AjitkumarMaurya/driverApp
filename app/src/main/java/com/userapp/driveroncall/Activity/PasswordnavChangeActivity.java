e83a43935571fd1417a901d100999f2d5e7bb05c
ajitmaurya <ajitmauray@gmail.com>
2019-03-09 15:37:21 +0530

    ajit09-03-2019

--- app/src/main/java/com/userapp/driveroncall/Activity/PasswordnavChangeActivity.java
+++ app/src/main/java/com/userapp/driveroncall/Activity/PasswordnavChangeActivity.java
@@ -2,7 +2,6 @@ package com.userapp.driveroncall.Activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
